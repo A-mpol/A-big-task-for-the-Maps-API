9543df8a15ee57d647b3c92fd81ddcec40acd6a6
polyana <a-mpol@yandex.ru>
2023-02-10 10:39:41 +0300

    Part #1

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
-import requests
+import os
 
+import requests
+import pygame
+
+coordinates = input()
+size = input()
+
+
+def geocode(coordinates, size):
+    map_request = 'http://static-maps.yandex.ru/1.x/?ll=' + coordinates + "&z=" + size + "&l=map"
+    response = requests.get(map_request)
+    return response.content
+
+
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(geocode(coordinates, size))
+
+try:
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    pygame.display.flip()
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+    pygame.quit()
+
+    os.remove(map_file)
+except Exception:
+    print('неверный тип данных')
