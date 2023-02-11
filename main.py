c7e722a140509c4c0b4786f238a596e4f7a046c9
Ksenia <kcy.2006@mail.ru>
2023-02-11 20:34:02 +0300

    part 1 with interface

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
 import os
+import sys
 
 import requests
-import pygame
-
-coordinates = input()
-size = input()
-
-
-def geocode(coordinates, size):
-    map_request = 'http://static-maps.yandex.ru/1.x/?ll=' + coordinates + "&z=" + size + "&l=map"
-    response = requests.get(map_request)
-    return response.content
+from PyQt5.QtGui import QPixmap
+from map_interface import Ui_MainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(geocode(coordinates, size))
+class Map(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.setWindowTitle("Map")
 
-try:
-    pygame.init()
-    screen = pygame.display.set_mode((600, 450))
-    screen.blit(pygame.image.load(map_file), (0, 0))
-    pygame.display.flip()
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
-    pygame.quit()
+        self.show_button.clicked.connect(self.set_image)
 
-    os.remove(map_file)
-except Exception:
-    print('неверный тип данных')
+    def geocode(self):
+        coordinates = self.coords_edit.text()
+        size = self.size_edit.text()
+        map_request = 'http://static-maps.yandex.ru/1.x/?ll=' + coordinates + "&z=" + size + "&l=map"
+        response = requests.get(map_request)
+        return response.content
+
+    def set_image(self):
+        self.map_file = "map.png"
+        with open(self.map_file, "wb") as file:
+            file.write(self.geocode())
+
+        self.pixmap = QPixmap(self.map_file)
+        self.image_label.setPixmap(self.pixmap)
+
+    def closeEvent(self, event):
+        os.remove(self.map_file)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Map()
+    ex.show()
+    sys.exit(app.exec_())
