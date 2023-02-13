ecdf5074af8af1cb672d1b449fb460fe7a59b723
Ksenia <kcy.2006@mail.ru>
2023-02-13 16:56:48 +0300

    part 2, mistake with PgUp and PgDn was fixed

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ class Map(QMainWindow, Ui_MainWindow):
         self.show_button.clicked.connect(self.set_image)
 
     def geocode(self):
-        coordinates = self.coordinates
-        size = self.size
-        map_request = 'http://static-maps.yandex.ru/1.x/?ll=' + coordinates + "&z=" + str(size) + "&l=map"
+        coordinates = str(self.coordinates[0]) + "," + str(self.coordinates[1])
+        size = str(self.size)
+        map_request = 'http://static-maps.yandex.ru/1.x/?ll=' + coordinates + "&z=" + size + "&l=map"
         response = requests.get(map_request)
         return response.content
 
@@ -39,7 +39,8 @@ class Map(QMainWindow, Ui_MainWindow):
     def check_parameters(self):
         self.mistake_label.setText("")
         try:
-            self.coordinates = self.coords_edit.text().replace(" ", "")
+            self.coordinates = self.coords_edit.text().replace(" ", "").split(",")
+            self.coordinates = tuple([float(self.coordinates[0]), float(self.coordinates[1])])
             self.size = int(self.size_edit.text())
             if not self.geocode():
                 raise ResponseError
@@ -48,17 +49,16 @@ class Map(QMainWindow, Ui_MainWindow):
             self.mistake_label.setText("Некорректные данные!")
             return False
 
-
     def closeEvent(self, event):
         os.remove(self.map_file)
 
     def keyPressEvent(self, event):
-        if event.key() == Qt.Key_Up:
+        if event.key() == Qt.Key_PageUp:
             if self.size < 19:
                 self.size += 1
                 self.size_edit.setText(str(self.size))
                 self.set_image()
-        elif event.key() == Qt.Key_Down:
+        elif event.key() == Qt.Key_PageDown:
             if self.size > 1:
                 self.size -= 1
                 self.size_edit.setText(str(self.size))
